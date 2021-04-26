3a3812d7ff9f41253a05e33d6139bee9d310aedc
greisane <ggreisane@gmail.com>
2021-04-26 16:09:28 -0300

    Remove debugging stuff from texture bake

--- material/texture_bake.py
+++ material/texture_bake.py
@@ -152,6 +152,109 @@ bake_items = [
     ('CURVATURE', "Curvature", "Curvature, centered on gray"),
 ]
 
+class GRET_OT_quick_unwrap(bpy.types.Operator):
+    #tooltip
+    """Smart unwrap and pack UVs for all objects that have the active material assigned"""
+
+    bl_idname = 'gret.quick_unwrap'
+    bl_label = "Quick Unwrap"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    uv_layer_name: bpy.props.StringProperty(
+        name="UV Layer",
+        description="""Name of the target UV layer.
+Defaults to the setting found in addon preferences if not specified""",
+        default="",
+    )
+    angle_limit: bpy.props.FloatProperty(
+        name="Angle Limit",
+        description="Lower for more projection groups, higher for less distortion",
+        subtype='ANGLE',
+        default=radians(66.0),
+        min=radians(0.0),
+        max=radians(89.0),
+    )
+    area_weight: bpy.props.FloatProperty(
+        name="Area Weight",
+        description="Weight projection vectors by faces with larger areas",
+        default=0.0,
+        min=0.0,
+        max=1.0,
+    )
+    align_with_world: bpy.props.BoolProperty(
+        name="Align With World",
+        description="Rotate UV islands to flow in the direction of gravity. Requires TexTools addon",
+        default=True,
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return context.object and context.object.active_material and context.mode == 'EDIT_MESH'
+
+    def execute(self, context):
+        mat = context.object.active_material
+        saved_area_ui_type = context.area.ui_type
+        saved_use_uv_select_sync = context.scene.tool_settings.use_uv_select_sync
+        saved_selection = save_selection()
+        saved_active_uv_layers = {}  # Object to UV layer
+        margin = 1.0 / 128 * 2
+        self.uv_layer_name = self.uv_layer_name or prefs.quick_unwrap_uv_layer_name
+
+        try:
+            # Select all faces of all objects that share the material
+            bpy.ops.object.editmode_toggle()
+            context.scene.tool_settings.use_uv_select_sync = True
+            objs = [o for o in context.scene.objects if mat.name in o.data.materials]
+            select_only(context, objs)
+            bpy.ops.object.editmode_toggle()
+            bpy.ops.mesh.reveal()
+            bpy.ops.mesh.select_mode(type='FACE')
+            bpy.ops.object.editmode_toggle()
+            for obj in objs:
+                saved_active_uv_layers[obj] = obj.data.uv_layers.active
+                uv = obj.data.uv_layers.get(self.uv_layer_name)
+                if not uv:
+                    uv = obj.data.uv_layers.new(name=self.uv_layer_name)
+                uv.active = True
+                for face in obj.data.polygons:
+                    face.select = obj.data.materials[face.material_index] == mat
+            bpy.ops.object.editmode_toggle()
+
+            # Unwrap
+            bpy.ops.uv.smart_project(
+                angle_limit=self.angle_limit,
+                island_margin=margin,
+                area_weight=self.area_weight,
+                correct_aspect=True,
+                scale_to_bounds=False)
+
+            # If set and TexTools is available, rotate islands
+            if self.align_with_world:
+                try:
+                    context.area.ui_type = 'UV'
+                    context.scene.tool_settings.use_uv_select_sync = False
+                    bpy.ops.uv.textools_island_align_world(steps=2)
+                except AttributeError:
+                    pass
+
+            # If available, pack using an addon
+            try:
+                context.scene.uvp2_props.margin = margin
+                context.scene.uvp2_props.rot_enable = not self.align_with_world
+                bpy.ops.uvpackmaster2.uv_pack()
+            except AttributeError:
+                pass
+        finally:
+            for obj, uv_layer in saved_active_uv_layers.items():
+                obj.data.uv_layers.active = uv_layer
+            load_selection(saved_selection)
+            context.scene.tool_settings.use_uv_select_sync = saved_use_uv_select_sync
+            context.area.ui_type = saved_area_ui_type
+            # Exiting edit mode here causes uvpackmaster2 to break, it's doing some weird modal stuff
+            # bpy.ops.object.mode_set(mode='OBJECT')
+
+        return {'FINISHED'}
+
 class GRET_OT_bake(bpy.types.Operator):
     #tooltip
     """Bake and export the texture.
@@ -167,20 +270,9 @@ All faces from all objects assigned to the active material are assumed to contri
 Defaults to the setting found in addon preferences if not specified""",
         default="",
     )
-    debug: bpy.props.BoolProperty(
-        name="Debug",
-        description="Debug mode with verbose output. Keeps intermediate materials and textures",
-        default=False,
-    )
 
     def new_image(self, name, size):
-        # For debugging purposes, try to reuse an image if it exists
-        image = bpy.data.images.get(name)
-        if image and image.size[:] != (size, size):
-            bpy.data.images.remove(image)
-            image = None
-        if not image:
-            image = bpy.data.images.new(name=name, width=size, height=size)
+        image = bpy.data.images.new(name=name, width=size, height=size)
         self.new_images.append(image)
 
         image.colorspace_settings.name = 'Linear'
@@ -188,11 +280,7 @@ Defaults to the setting found in addon preferences if not specified""",
         return image
 
     def new_bake_material(self, image):
-        # For debugging purposes, try to reuse a material if it exists
-        name = f"_bake{image.name}"
-        mat = bpy.data.materials.get(name)
-        if not mat:
-            mat = bpy.data.materials.new(name=name)
+        mat = bpy.data.materials.new(name=f"_bake{image.name}")
         self.new_materials.append(mat)
 
         mat.use_nodes = True
@@ -317,11 +405,10 @@ Defaults to the setting found in addon preferences if not specified""",
             beep(pitch=3, num=1)
         finally:
             # Clean up
-            if not self.debug:
-                while self.new_materials:
-                    bpy.data.materials.remove(self.new_materials.pop())
-                while self.new_images:
-                    bpy.data.images.remove(self.new_images.pop())
+            while self.new_materials:
+                bpy.data.materials.remove(self.new_materials.pop())
+            while self.new_images:
+                bpy.data.images.remove(self.new_images.pop())
             for obj, matrix_world in self.saved_transforms.items():
                 obj.matrix_world = matrix_world
             del self.saved_transforms
@@ -333,112 +420,6 @@ Defaults to the setting found in addon preferences if not specified""",
             context.scene.cycles.samples = saved_cycles_samples
             context.preferences.edit.use_global_undo = saved_use_global_undo
             logger.end_logging()
-
-        if self.debug:
-            bpy.ops.ed.undo_push()
-
-        return {'FINISHED'}
-
-class GRET_OT_quick_unwrap(bpy.types.Operator):
-    #tooltip
-    """Smart unwrap and pack UVs for all objects that have the active material assigned"""
-
-    bl_idname = 'gret.quick_unwrap'
-    bl_label = "Quick Unwrap"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    uv_layer_name: bpy.props.StringProperty(
-        name="UV Layer",
-        description="""Name of the target UV layer.
-Defaults to the setting found in addon preferences if not specified""",
-        default="",
-    )
-    angle_limit: bpy.props.FloatProperty(
-        name="Angle Limit",
-        description="Lower for more projection groups, higher for less distortion",
-        subtype='ANGLE',
-        default=radians(66.0),
-        min=radians(0.0),
-        max=radians(89.0),
-    )
-    area_weight: bpy.props.FloatProperty(
-        name="Area Weight",
-        description="Weight projection vectors by faces with larger areas",
-        default=0.0,
-        min=0.0,
-        max=1.0,
-    )
-    align_with_world: bpy.props.BoolProperty(
-        name="Align With World",
-        description="Rotate UV islands to flow in the direction of gravity. Requires TexTools addon",
-        default=True,
-    )
-
-    @classmethod
-    def poll(cls, context):
-        return context.object and context.object.active_material and context.mode == 'EDIT_MESH'
-
-    def execute(self, context):
-        mat = context.object.active_material
-        saved_area_ui_type = context.area.ui_type
-        saved_use_uv_select_sync = context.scene.tool_settings.use_uv_select_sync
-        saved_selection = save_selection()
-        saved_active_uv_layers = {}  # Object to UV layer
-        margin = 1.0 / 128 * 2
-        self.uv_layer_name = self.uv_layer_name or prefs.quick_unwrap_uv_layer_name
-
-        try:
-            # Select all faces of all objects that share the material
-            bpy.ops.object.editmode_toggle()
-            context.scene.tool_settings.use_uv_select_sync = True
-            objs = [o for o in context.scene.objects if mat.name in o.data.materials]
-            select_only(context, objs)
-            bpy.ops.object.editmode_toggle()
-            bpy.ops.mesh.reveal()
-            bpy.ops.mesh.select_mode(type='FACE')
-            bpy.ops.object.editmode_toggle()
-            for obj in objs:
-                saved_active_uv_layers[obj] = obj.data.uv_layers.active
-                uv = obj.data.uv_layers.get(self.uv_layer_name)
-                if not uv:
-                    uv = obj.data.uv_layers.new(name=self.uv_layer_name)
-                uv.active = True
-                for face in obj.data.polygons:
-                    face.select = obj.data.materials[face.material_index] == mat
-            bpy.ops.object.editmode_toggle()
-
-            # Unwrap
-            bpy.ops.uv.smart_project(
-                angle_limit=self.angle_limit,
-                island_margin=margin,
-                area_weight=self.area_weight,
-                correct_aspect=True,
-                scale_to_bounds=False)
-
-            # If set and TexTools is available, rotate islands
-            if self.align_with_world:
-                try:
-                    context.area.ui_type = 'UV'
-                    context.scene.tool_settings.use_uv_select_sync = False
-                    bpy.ops.uv.textools_island_align_world(steps=2)
-                except AttributeError:
-                    pass
-
-            # If available, pack using an addon
-            try:
-                context.scene.uvp2_props.margin = margin
-                context.scene.uvp2_props.rot_enable = not self.align_with_world
-                bpy.ops.uvpackmaster2.uv_pack()
-            except AttributeError:
-                pass
-        finally:
-            for obj, uv_layer in saved_active_uv_layers.items():
-                obj.data.uv_layers.active = uv_layer
-            load_selection(saved_selection)
-            context.scene.tool_settings.use_uv_select_sync = saved_use_uv_select_sync
-            context.area.ui_type = saved_area_ui_type
-            # Exiting edit mode here causes uvpackmaster2 to break, it's doing some weird modal stuff
-            # bpy.ops.object.mode_set(mode='OBJECT')
 
         return {'FINISHED'}
 
@@ -514,7 +495,7 @@ class GRET_PT_texture_bake(bpy.types.Panel):
 
     @classmethod
     def poll(cls, context):
-        return context.object and context.object.active_material
+        return context.active_object and context.active_object.active_material
 
     def draw(self, context):
         layout = self.layout
@@ -538,9 +519,6 @@ class GRET_PT_texture_bake(bpy.types.Panel):
         row = col.row(align=True)
         row.operator('gret.quick_unwrap', icon='UV')
         op = row.operator('gret.bake', icon='INDIRECT_ONLY_ON', text="Bake")
-        op.debug = False
-        op = row.operator('gret.bake', icon='INDIRECT_ONLY_OFF', text="")
-        op.debug = True
 
 class GRET_PG_texture_bake(bpy.types.PropertyGroup):
     size: bpy.props.IntProperty(
