greisane/gret

commit 9c86bc50beead80ff1580a40f8df9c6237143ed3

Fix keymaps not removed on unregistering tile paint

material/tile_paint.py
@@ -550,18 +550,18 @@ def register_keymaps():
 
 def unregister_keymaps():
     keyconfigs = bpy.context.window_manager.keyconfigs
-    defaultmap = keyconfigs.get('blender').keymaps
-    addonmap = keyconfigs.get('blender addon').keymaps
+    kc_defaultconf = keyconfigs.default
+    kc_addonconf = keyconfigs.addon
 
     for km_name, km_args, km_content in [km_tool_paint]:
-        keymap = addonmap.find(km_name, **km_args)
+        keymap = kc_addonconf.find(km_name, **km_args)
         keymap_items = keymap.keymap_items
         for item in km_content['items']:
             item_id = keymap_items.find(item[0])
             if item_id != -1:
                 keymap_items.remove(keymap_items[item_id])
-        addonmap.remove(keymap)
-        defaultmap.remove(defaultmap.find(km_name, **km_args))
+        kc_addonconf.remove(keymap)
+        kc_defaultconf.remove(kc_defaultconf.find(km_name, **km_args))
 
 def clear_tilesets():
     for tileset in tilesets.values():

commit e613fe8db9f14a88ada05d811fd325239ae364d9

Fix typo introduced in 9c86bc50beead80ff1580a40f8df9c6237143ed3

material/tile_paint.py
@@ -542,26 +542,26 @@ km_tool_paint_empty = keymap_view3d_empty(km_tool_paint_name)
 
 def register_keymaps():
     keyconfigs = bpy.context.window_manager.keyconfigs
-    kc_defaultconf = keyconfigs.default
-    kc_addonconf = keyconfigs.addon
+    kc_default = keyconfigs.default
+    kc_addon = keyconfigs.addon
 
-    keyconfig_init_from_data(kc_defaultconf, [km_tool_paint_empty])
-    keyconfig_init_from_data(kc_addonconf, [km_tool_paint])
+    keyconfig_init_from_data(kc_default, [km_tool_paint_empty])
+    keyconfig_init_from_data(kc_addon, [km_tool_paint])
 
 def unregister_keymaps():
     keyconfigs = bpy.context.window_manager.keyconfigs
-    kc_defaultconf = keyconfigs.default
-    kc_addonconf = keyconfigs.addon
+    kc_default_keymaps = keyconfigs.default.keymaps
+    kc_addon_keymaps = keyconfigs.addon.keymaps
 
     for km_name, km_args, km_content in [km_tool_paint]:
-        keymap = kc_addonconf.find(km_name, **km_args)
+        keymap = kc_addon_keymaps.find(km_name, **km_args)
         keymap_items = keymap.keymap_items
         for item in km_content['items']:
             item_id = keymap_items.find(item[0])
             if item_id != -1:
                 keymap_items.remove(keymap_items[item_id])
-        kc_addonconf.remove(keymap)
-        kc_defaultconf.remove(kc_defaultconf.find(km_name, **km_args))
+        kc_addon_keymaps.remove(keymap)
+        kc_default_keymaps.remove(kc_default_keymaps.find(km_name, **km_args))
 
 def clear_tilesets():
     for tileset in tilesets.values():