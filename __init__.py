3311fa2eed2dc487f5e3ebfb30bdf38e6e6e11b3
greisane <ggreisane@gmail.com>
2021-10-30 07:18:51 -0300

    Remove ancient backwards compatibility option

--- __init__.py
+++ __init__.py
@@ -37,7 +37,6 @@ module_names = [
     'helpers',
     'math',
     'rbf',
-    # 'stringcase',  # Third party, no need to register or reload
     'file',
     'material',
     'mesh',
@@ -73,11 +72,6 @@ class GretAddonPreferences(bpy.types.AddonPreferences):
         description="Automatically hide the ARP armature collection on saving",
         default=False,
     )
-    backwards_compat: bpy.props.BoolProperty(
-        name="Backwards Compatibility",
-        description="Restore settings from when the addon was called my_tools",
-        default=False,
-    )
     debug: bpy.props.BoolProperty(
         name="Debug Mode",
         description="Enables verbose output",
@@ -89,7 +83,6 @@ class GretAddonPreferences(bpy.types.AddonPreferences):
         col = layout.column(align=True)
 
         col.prop(self, 'jobs_panel_enable')
-        col.prop(self, 'backwards_compat')
         col.prop(self, 'auto_hide_arp_enable')
         col.prop(self, 'debug')
         col.separator()
@@ -109,41 +102,6 @@ class GRET_PG_settings(bpy.types.PropertyGroup):
             cls.__annotations__ = {}
         cls.__annotations__[name] = annotation
 
-# Backwards compatibility if enabled in preferences
-@persistent
-def load_pre(dummy):
-    bpy.types.Scene.my_tools = bpy.props.PointerProperty(type=GRET_PG_settings)
-
-@persistent
-def load_post(dummy):
-    if not hasattr(bpy.types.Scene, 'my_tools'):
-        return
-    from .helpers import is_defaulted, save_properties, load_properties
-    for scene in bpy.data.scenes:
-        if 'my_tools' in scene and not is_defaulted(scene.my_tools):
-            print("Found old gret settings in file, restoring")
-            load_properties(scene.gret, save_properties(scene.my_tools))
-            # Fix up holes in old job settings
-            for job in scene.gret.export_jobs:
-                for index in range(len(job.collections) - 2, -1, -1):
-                    item = job.collections[index]
-                    if not item.collection:
-                        job.collections.remove(index)
-                for index in range(len(job.actions) - 2, -1, -1):
-                    item = job.actions[index]
-                    if not item.action and not item.use_pattern:
-                        job.actions.remove(index)
-                for index in range(len(job.copy_properties) - 2, -1, -1):
-                    item = job.copy_properties[index]
-                    if not item.source and not item.destination:
-                        job.copy_properties.remove(index)
-                for index in range(len(job.remap_materials) - 2, -1, -1):
-                    item = job.remap_materials[index]
-                    if not item.source and not item.destination:
-                        job.remap_materials.remove(index)
-            del scene['my_tools']
-    del bpy.types.Scene.my_tools
-
 classes = (
     GRET_PG_settings,
     GretAddonPreferences,
@@ -161,15 +119,7 @@ def register():
 
     bpy.types.Scene.gret = bpy.props.PointerProperty(type=GRET_PG_settings)
 
-    if prefs.backwards_compat:
-        bpy.app.handlers.load_pre.append(load_pre)
-        bpy.app.handlers.load_post.append(load_post)
-
 def unregister():
-    if prefs.backwards_compat:
-        bpy.app.handlers.load_pre.remove(load_pre)
-        bpy.app.handlers.load_post.remove(load_post)
-
     del bpy.types.Scene.gret
 
     for cls in reversed(classes):
